9ebf43d6d4406946139b158605c392f070b851de
Jan <jan.loewenstrom@web.de>
2019-08-31 11:56:17 +0200

    select channels for input; select SampleRange;
    Former-commit-id: cb934b8934609074a6714b61ea9b7a1adedb869d

--- leftRightMLP.py
+++ leftRightMLP.py
@@ -10,71 +10,57 @@ from keras.utils import np_utils
 seed = 9
 np.random.seed(seed)
 
-dataset = np.loadtxt("data/leftRight.csv", delimiter=",", skiprows=1)
+dataset = np.loadtxt("data/blueRedMovement.csv", delimiter=",", skiprows=1)
 # split into input (X) and output (Y) variables
-X = dataset[:, 0:8]
+number_of_channels = 3
+X = dataset[:, 5:8]
 Y = dataset[:, 8]
-SampleRange = 75
-prev = Y[0]
+SampleRange = 5
 matrix = []
 labels = []
-for i in range(0, len(X)-SampleRange, 1):
-    if Y[i] != prev:
-        labels.append(prev)
-        prev = Y[i]
-        frame = []
-        for j in range(0, SampleRange):
-            frame.append(X[i - SampleRange + j])
-        matrix.append(np.column_stack(tuple(frame)))
 
+i = 0
+while i < len(X) - SampleRange:
+    frame = []
+    for j in range(0, SampleRange):
+        frame.append(X[i + j])
+    matrix.append(frame)
+    i += SampleRange
+    labels.append(Y[i])
 
 X = np.array(matrix)
-print(X.shape)
-X = np.expand_dims(X, axis=3)
+Y = np.array(labels)
+
+#X = np.expand_dims(X, axis=3)
 #X = np.reshape(X.shape[0], 1, 8, SampleRange)
-print(X.shape)
 
 # standardizing the input feature
 sc = StandardScaler()
 # X = sc.fit_transform(X)
-Y = np.array(labels)
-print(Y.shape)
+
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3)
 
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
+
 num_classes = y_train.shape[1]
 print(num_classes)
+
+X_train = np.reshape(X_train, [-1, number_of_channels*SampleRange])
+X_test = np.reshape(X_test, [-1, number_of_channels*SampleRange])
+
 classifier = Sequential()
-classifier.add(Conv2D(75, (5, 5), input_shape=(8, SampleRange, 1), activation='relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-classifier.add(Conv2D(35, (5, 3), activation='relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-classifier.add(Dropout(0.2))
-classifier.add(Flatten())
-classifier.add(Dense(128, activation='relu'))
-classifier.add(Dense(50, activation='relu'))
+classifier.add(Dense(32, input_shape=(number_of_channels*SampleRange,)))
 classifier.add(Dense(num_classes, activation='softmax'))
-
-#classifier.add(Conv2D(64, (3, 3), input_shape=(8, SampleRange, 1)))
-#classifier.add(Flatten())
-# First Hidden Layer
-#classifier.add(Dense(8, activation='relu', kernel_initializer='random_normal'))
-# Second  Hidden Layer
-#classifier.add(Dense(8, activation='relu', kernel_initializer='random_normal'))
-# Output Layer
-#classifier.add(Dense(num_classes, activation='softmax', kernel_initializer='random_normal'))
-# Compiling the neural network
-#classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
+classifier.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
 # Fitting the data to the training dataset
-classifier.fit(X_train, y_train, batch_size=64, epochs=20, validation_data=(X_test, y_test))
+classifier.fit(X_train, y_train, batch_size=32, epochs=6, validation_data=(X_test, y_test))
 
 # Final evaluation of the model
 eval_model = classifier.evaluate(X_train, y_train)
 print("Baseline Error: %.2f%%" % (100 - eval_model[1] * 100))
-print(eval_model[0])
-print(eval_model[1])
+print(eval_model[0] * 100)
+print(eval_model[1] * 100)
 
 # Save the model
 # serialize model to JSON
