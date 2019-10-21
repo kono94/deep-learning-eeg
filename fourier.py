b70e5d415cc7545c7a5b3a6a0a89f62ec393abc5
Jan Löwenstrom <jan.loewenstrom@web.de>
2019-10-21 19:52:38 +0200

    variable selection of channels, much refactoring
    new dataset
    
    
    Former-commit-id: 6daec8251f1cc081f2d9813efb8e3b85229a6469

--- fourier.py
+++ fourier.py
@@ -43,79 +43,81 @@ def normalize(data):
     return np.array(y)
 
 
-dataset = np.loadtxt("data/leftRight.csv", delimiter=",", skiprows=1)
-# Choose electrodes
-X = dataset[:, 3:5]
+channelsToUse = [2,3,4]
+numberOfChannels = len(channelsToUse)
+classes = 3
+X = np.loadtxt("data/2019-10-16-17-30_MaximilianWernerMitHand.csv", usecols=channelsToUse, delimiter=",", skiprows=1)
 # get label
-Y = dataset[:, 8]
-
+Y = np.loadtxt("data/2019-10-16-17-30_MaximilianWernerMitHand.csv", usecols=[8],dtype=np.str, delimiter=",", skiprows=1)
 # split data to corresponding label
-X0 = []
-X1 = []
-X2 = []
-X0b = []
-X1b = []
-X2b = []
+print(Y)
+fullX = []
+for i in range(0, numberOfChannels, 1):
+    l = []
+    for i in range(0, classes, 1):
+        k = []
+        l.append(k)
+    fullX.append(l)
 
 for i in range(0, len(X), 1):
-    if Y[i] == 1:
-        X1.append(X[i][0])
-        X1b.append(X[i][1])
-    elif Y[i] == 2:
-        X2.append(X[i][0])
-        X2b.append(X[i][1])
-    elif Y[i] == 0:
-        X0.append(X[i][0])
-        X0b.append(X[i][1])
+    classIndex = 0
+    if Y[i] == "none":
+       classIndex = 0
+    elif Y[i] == "left":
+       classIndex = 1
+    elif Y[i] == "right":
+       classIndex = 2
+    for c in range(0, len(fullX), 1):
+        fullX[c][classIndex].append(X[i][c])
 
-X_list = []
-X_list.append(X0)
-X_list.append(X1)
-X_list.append(X2)
-X_list.append(X0b)
-X_list.append(X1b)
-X_list.append(X2b)
-
-X_list_filtered = []
-for i in range(0, len(X_list), 1):
-    data = X_list[i]
-    N = len(data)
-    fs = 250
-    L = N / fs
-    # remove 0.5 hz DC-offset
-    y = butter_highpass_filter(data, 0.5, fs)
-    # remove 50 hz power line frequency
-    y = notch_filter(y, 50, fs)
-    # extract frequencies between 2 and 60 hz
-    # typical characteristics for motor imagery
-    y = butter_bandpass_filter(y, 2, 60, fs)
-    # remove high voltage spikes with six sigma clipping
-    # +- sigma (default is 4, master thesis uses 6)
-    y = six_sigma_clipping(y, 6, 6)
-    # normalize each session and eletrode
-    y = normalize(y)
-    X_list_filtered.append(y)
+for c in range(0, len(fullX), 1):
+    for i in range(0, classes, 1):
+        data = fullX[c][i]
+        N = len(data)
+        fs = 250
+        L = N / fs
+        # remove 0.5 hz DC-offset
+        y = butter_highpass_filter(data, 0.5, fs)
+        # remove 50 hz power line frequency
+        y = notch_filter(y, 50, fs)
+        # extract frequencies between 2 and 60 hz
+        # typical characteristics for motor imagery
+        y = butter_bandpass_filter(y, 2, 60, fs)
+        # remove high voltage spikes with six sigma clipping
+        # +- sigma (default is 4, master thesis uses 6)
+        y = six_sigma_clipping(y, 6, 6)
+        # normalize each session and eletrode
+        y = normalize(y)
+        fullX[c][i] = y
 
 
 # data augmentation, generating crops with windows size 512 and shift by 8 samples
-mX = np.empty((int((26*512)/8*3), 2, 512))
-mY = np.zeros(shape=(int((26*512)/8*3), 3)) 
+miniumSampleSize = len(fullX[0][0])
+for i in range(0, classes, 1):
+    if(miniumSampleSize > len(fullX[0][i])):
+        miniumSampleSize = len(fullX[0][i])
 
-k = 0
-for i in range(0, 26*512-1, 8):
-    for j in range(0, 512, 1):
-        mX[k][0][j] = X_list_filtered[0][i+j]
-        mX[k+1][0][j] = X_list_filtered[1][i+j]
-        mX[k+2][0][j] = X_list_filtered[2][i+j]
+windowShift = 8
+windowSize = 512
+dataSetSize = int((miniumSampleSize - windowSize)/windowShift + 1)
+mX = np.empty(shape=(dataSetSize, numberOfChannels, windowSize))
+mY = np.zeros(shape=(dataSetSize, classes))
+print(dataSetSize)
+print(miniumSampleSize)
+# [0] - [0]  0,4 0,3 0,4
+#       [1]  0,3 0,3 0,3
+#       [2]  0,2 0,2 0,2
+# [1] - [0]  0,1 0,2 ,2 
+#       [1]
+#       [2]
 
-        mX[k][1][j] = X_list_filtered[3][i+j]
-        mX[k+1][1][j] = X_list_filtered[4][i+j]
-        mX[k+2][1][j] = X_list_filtered[5][i+j]
-
-        mY[k] = [1,0,0]
-        mY[k+1] = [0,1,0]
-        mY[k+2] = [0,0,1]
-    k += 3
+for i in range(0, dataSetSize,  classes):
+    for w in range(0, windowSize, 1):
+        for ch in range(0, numberOfChannels, 1):
+            for cl in range(0, classes, 1):
+                mX[i + cl][ch][w] = fullX[ch][cl][i * windowShift + w]
+                mY[i + cl] = [0, 0, 0]
+                mY[i + cl][cl] = 1
 
 plt.xlabel("Sample")
 plt.ylabel("Voltage in mV")
@@ -123,10 +125,10 @@ plt.plot(mX[0][0])
 plt.show()
 
 # generate spectogram
-windowSize = 128
-step = 8
-freqs, times, Sx = signal.spectrogram(mX[0][0], fs=fs, window='hanning', nperseg=windowSize,
-			 noverlap=windowSize - step, detrend=False, scaling='spectrum')
+spectroWindowSize = 128
+spectroStep = 8
+freqs, times, Sx = signal.spectrogram(mX[0][0], fs=fs, window='hanning', nperseg=spectroWindowSize,
+			 noverlap=spectroWindowSize - spectroStep, detrend=False, scaling='spectrum')
 
 
 f, ax = plt.subplots(figsize=(10, 5))
@@ -137,12 +139,11 @@ plt.show()
 
 #create model
 model = Sequential()
-# 3 channels, 4-sec input
-numberOfChannels = 2
-classes = 3
-input_shape = (numberOfChannels, 512)
-
-model.add(Spectrogram(input_shape=input_shape, n_dft=128, n_hop=8, padding='same',
+# 2-sec input
+input_shape = (numberOfChannels, windowSize)
+spectroWindowSize = 128 
+spectroWindowShift = 8
+model.add(Spectrogram(input_shape=input_shape, n_dft=spectroWindowSize, n_hop=spectroWindowShift, padding='same',
                                  power_spectrogram=2.0, return_decibel_spectrogram=True))
 #add model layers
 #1
@@ -150,21 +151,21 @@ model.add(Conv2D(24, kernel_size=(12,12), input_shape=(64, 64, numberOfChannels)
 model.add(BatchNormalization())
 model.add(MaxPool2D(pool_size=(2,2)))
 model.add(ReLU())
-model.add(Dropout(rate=0.5))
+model.add(Dropout(rate=0.2))
 
 #2
 model.add(Conv2D(48, kernel_size=(8,8), activation='relu'))
 model.add(BatchNormalization())
 model.add(MaxPool2D(pool_size=(2,2)))
 model.add(ReLU())
-model.add(Dropout(rate=0.5))
+model.add(Dropout(rate=0.2))
 
 #3
 model.add(Conv2D(96, kernel_size=(4,4), activation='relu'))
 model.add(BatchNormalization())
 model.add(MaxPool2D(pool_size=(2,2)))
 model.add(ReLU())
-model.add(Dropout(rate=0.5))
+model.add(Dropout(rate=0.2))
 
 #4
 model.add(Flatten())
@@ -177,7 +178,11 @@ rng_state = np.random.get_state()
 np.random.shuffle(mX)
 np.random.set_state(rng_state)
 np.random.shuffle(mY)
-model.fit(mX[0:3000], mY[0:3000], batch_size=256, epochs=10, validation_data=(mX[3000:4000], mY[3000:4000]))
+
+trainSplit = 0.8
+batch = 128
+epoch = 20
+model.fit(mX[0:int(dataSetSize*0.8)], mY[0:int(dataSetSize*0.8)], batch_size=batch, epochs=epoch, validation_data=(mX[int(dataSetSize*0.8):int(dataSetSize-1)], mY[int(dataSetSize*0.8):int(dataSetSize-1)]))
 
 model.summary()
 model.save('temp_model.h5')
@@ -187,40 +192,7 @@ model2 = load_model('temp_model.h5',
   custom_objects={'Spectrogram':Spectrogram})
 model2.summary()
 
-p = np.array([mX[4200]])
-q = mY[4200]
-pred1 = model2.predict_classes(p)
-prob1 = model2.predict(p)
-print("should predict ", q, " predicted", pred1, " Prob", prob1)
-
-p = np.array([mX[4300]])
-q = mY[4300]
-pred1 = model2.predict_classes(p)
-prob1 = model2.predict(p)
-print("should predict ", q, " predicted", pred1, " Prob", prob1)
-
-
-p = np.array([mX[4400]])
-q = mY[4400]
-pred1 = model2.predict_classes(p)
-prob1 = model2.predict(p)
-print("should predict ", q, " predicted", pred1, " Prob", prob1)
-
-
-p = np.array([mX[4403]])
-q = mY[4403]
-pred1 = model2.predict_classes(p)
-prob1 = model2.predict(p)
-print("should predict ", q, " predicted", pred1, " Prob", prob1)
-
-
-p = np.array([mX[4406]])
-q = mY[4406]
-pred1 = model2.predict_classes(p)
-prob1 = model2.predict(p)
-print("should predict ", q, " predicted", pred1, " Prob", prob1)
-
-for i in range(4000, 4100, 1):
+for i in range(int(dataSetSize*0.8), int(dataSetSize), 1):
     p = np.array([mX[i]])
     q = mY[i]
     pred1 = model.predict_classes(p)
